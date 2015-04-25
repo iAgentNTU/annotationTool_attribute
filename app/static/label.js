f650e31de80ed48f545e1a95b2f6f2fc9edfdfd8
Meng-Ying Chan <himeng0130@gmail.com>
2015-04-25 15:43:24 +0800

    first commit

---
+++ app/static/label.js
@@ -0,0 +1,127 @@
+var choice = 'None';
+var label_post = 'None';
+var reason = 'None';
+var category = ['Meeting', 'Lecture', 'Study', 'Empty'];
+var expression = ['開會', '上課', '自習/休閒', '無活動'];
+const GENERAL = '#33FFFF';
+const CHOSEN = '#91C7FF';
+
+
+// get label_post from he left side
+
+document.addEventListener("keyup", keyup, false);
+function keyup(event){
+	var value = document.getElementById('reason').value;
+	if(value == "") value = 'None';
+	reason = value;
+	//document.getElementById('word1').innerHTML = value;
+	//document.getElementById('open').style.display = 'none';
+}
+
+function setdefaultvalue(label){
+	label_post = label;
+	document.getElementById('open').style.display = 'none';
+}
+
+function clearchoice(){
+	choice = 'None';
+	label_post = 'None';
+	$('.choice').removeClass('active');
+	document.getElementById('open').style.display = 'none';
+}
+
+function setupchoice(button){
+	choice = button.getAttribute("value");
+	label_post = choice;
+	if(choice == 'Others')
+		document.getElementById('open').style.display = 'block';
+	$(button).addClass("active");
+}
+
+function light(button){
+	if(choice == button.getAttribute("value")){
+		clearchoice();
+	}else{
+		clearchoice();
+		setupchoice(button);
+	}
+}
+
+function refreshchoice(){
+	choice = 'None';
+	label_post = 'None';
+	reason = 'None';
+	//document.getElementById('next').innerHTML = 'Next';
+	//document.getElementById('word1').innerHTML = '';
+	document.getElementById('reason').value = "";
+	clearchoice();
+}
+
+
+// main part
+
+var timestamp;
+
+function replace(page){
+	document.write(page);
+	document.close();
+}
+
+function record(){
+	console.log(label_post);
+	console.log(reason);
+	if(label_post == 'None'){
+		alert('請選擇一個活動類別');
+		return;
+	}
+	if(reason == 'None' || reason == ""){
+		alert('請填寫判斷的依據');
+		return;
+	}
+	var timediff = new Date().getTime() - timestamp;
+	var pic = document.getElementById('pic');
+	$.post("/record/"+pic.getAttribute('value')+"/"+label_post+"/"+timediff+"/"+reason, function(response){
+		//console.log(response);
+		//console.log(response.pic);
+		if(typeof(response) == 'string') replace(response);
+		setpic(response.pic, response.idx, response.ttl);
+	});
+	refreshchoice();
+}
+
+function show(s, idx, ttl){
+	return '('+idx+'/'+ttl+')  '+s.substring(0,4)+'/'+s.substring(4,6)+'/'+s.substring(6,8)+' '+s.substring(9,11)+':'+s.substring(11,13);
+}
+
+function setpic(newpic, newidx, ttlidx){
+	picObj = document.getElementById('pic');
+	picObj.setAttribute("value", newpic);
+	picObj.src = "http://disa.csie.ntu.edu.tw/~janetyc/data/"+newpic.substring(0,8)+"/image_"+newpic+".jpg";
+	document.getElementById('time').innerHTML = show(newpic, newidx, ttlidx);
+	timestamp = new Date().getTime();
+	/*
+	picObj.onerror = function(){ 
+		$.getJSON('/newpic', function(response){
+			setpic(response.pic);
+		});
+	}
+	*/
+}
+
+
+// initiating
+		
+function buildup(){
+	upper = document.getElementById('upper');
+	choicecontainer = document.createElement("div");
+	choicecontainer.setAttribute("class", "btn-group");
+	for(i=0; i<category.length; ++i){
+		middle = document.createElement("button");
+		middle.setAttribute("class", "choice btn btn-lg btn-default");
+		middle.setAttribute("value", category[i]);
+		middle.setAttribute("onclick", "light(this)");
+		middle.innerHTML = expression[i];
+		choicecontainer.appendChild(middle);
+	}
+	upper.appendChild(choicecontainer);
+}
---
+++ app/util.py
@@ -0,0 +1,48 @@
+import random
+from sqlalchemy import func
+from . import db
+from models import *
+
+
+def get_shuffled_id():
+    print Pictures.query.count()
+    num = db.session.query(func.max(Pictures.id))[0][0]
+    print num
+    # num = 10000
+    piclist = [str(i+1) for i in range(num)]
+    random.shuffle(piclist)
+    # print piclist
+    return ','.join(piclist)
+
+'''
+def getpic(userid):
+    user = User.query.filter_by(id=userid)[0]
+    pool = str(user.picturepool).split(',')
+    try:
+        newidx = int(pool.pop(0))
+        user.picturepool = ','.join(pool)
+    except ValueError:
+        return None
+    # db.session.commit()
+    # print 'the first is', newidx
+    try:
+        nextpic = Pictures.query.filter_by(id=newidx)[0].pic
+        return nextpic
+    except IndexError:
+        return getpic(userid)
+'''
+def getpic(userid):
+    user = User.query.filter_by(id=userid)[0]
+    progress = user.progress
+    start = user.start
+    total = user.total
+    picNum = (progress+start+1) % 800
+    if progress < total:
+        nextpic = Pictures.query.filter_by(id=picNum)[0].pic
+        return nextpic, progress+1, total
+    else:
+        return None, None, None
+
+if __name__ == '__main__':
+    for i in range(50):
+        print getpic()
